a9131bf49208ec81a0ba7ba02b68726a733de64c
karim242 <karimelsa38@gmail.com>
2024-03-18 00:44:02 +0200

    generate assets image

---
+++ lib/constants/assets.dart
@@ -0,0 +1,120 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvater
+  /// assets/images/avater.svg
+  static const String imagesAvater = "assets/images/avater.svg";
+
+  /// Assets for imagesBeatsStudio
+  /// assets/images/beats_studio.svg
+  static const String imagesBeatsStudio = "assets/images/beats_studio.svg";
+
+  /// Assets for imagesCart
+  /// assets/images/cart.svg
+  static const String imagesCart = "assets/images/cart.svg";
+
+  /// Assets for imagesContactus
+  /// assets/images/contactus.svg
+  static const String imagesContactus = "assets/images/contactus.svg";
+
+  /// Assets for imagesDailyDeal2
+  /// assets/images/daily_deal2.svg
+  static const String imagesDailyDeal2 = "assets/images/daily_deal2.svg";
+
+  /// Assets for imagesDailyDeal3
+  /// assets/images/daily_deal3.svg
+  static const String imagesDailyDeal3 = "assets/images/daily_deal3.svg";
+
+  /// Assets for imagesDailyDeal4
+  /// assets/images/daily_deal4.svg
+  static const String imagesDailyDeal4 = "assets/images/daily_deal4.svg";
+
+  /// Assets for imagesDailyDeal5
+  /// assets/images/daily_deal5.svg
+  static const String imagesDailyDeal5 = "assets/images/daily_deal5.svg";
+
+  /// Assets for imagesDailyDeal6
+  /// assets/images/daily_deal6.svg
+  static const String imagesDailyDeal6 = "assets/images/daily_deal6.svg";
+
+  /// Assets for imagesDailyDeals1
+  /// assets/images/daily_deals1.svg
+  static const String imagesDailyDeals1 = "assets/images/daily_deals1.svg";
+
+  /// Assets for imagesExplore
+  /// assets/images/explore.svg
+  static const String imagesExplore = "assets/images/explore.svg";
+
+  /// Assets for imagesFilledHeart
+  /// assets/images/filled_heart.svg
+  static const String imagesFilledHeart = "assets/images/filled_heart.svg";
+
+  /// Assets for imagesGroup 54
+  /// assets/images/Group 54.svg
+  static const String imagesGroup54 = "assets/images/Group54.svg";
+
+  /// Assets for imagesGroup1
+  /// assets/images/Group1.svg
+  static const String imagesGroup1 = "assets/images/Group1.svg";
+
+  /// Assets for imagesGroup2
+  /// assets/images/Group2.svg
+  static const String imagesGroup2 = "assets/images/Group2.svg";
+
+  /// Assets for imagesGroup3
+  /// assets/images/Group3.svg
+  static const String imagesGroup3 = "assets/images/Group3.svg";
+
+  /// Assets for imagesGroup4
+  /// assets/images/Group4.svg
+  static const String imagesGroup4 = "assets/images/Group4.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesMask group
+  /// assets/images/Mask group.svg
+  static const String imagesMaskgroup = "assets/images/Maskgroup.svg";
+
+  /// Assets for imagesMessage
+  /// assets/images/message.svg
+  static const String imagesMessage = "assets/images/message.svg";
+
+  /// Assets for imagesNeedHelp
+  /// assets/images/need_help.svg
+  static const String imagesNeedHelp = "assets/images/need_help.svg";
+
+  /// Assets for imagesNewestSellers
+  /// assets/images/newest_sellers.svg
+  static const String imagesNewestSellers = "assets/images/newest_sellers.svg";
+
+  /// Assets for imagesPopularBrands
+  /// assets/images/popular_brands.svg
+  static const String imagesPopularBrands = "assets/images/popular_brands.svg";
+
+  /// Assets for imagesPrchaseHistory
+  /// assets/images/prchase_history.svg
+  static const String imagesPrchaseHistory = "assets/images/prchase_history.svg";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.svg
+  static const String imagesProfile = "assets/images/profile.svg";
+
+  /// Assets for imagesSaved
+  /// assets/images/saved.svg
+  static const String imagesSaved = "assets/images/saved.svg";
+
+  /// Assets for imagesSelling
+  /// assets/images/selling.svg
+  static const String imagesSelling = "assets/images/selling.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesSoundix
+  /// assets/images/soundix.svg
+  static const String imagesSoundix = "assets/images/soundix.svg";
+}
+
